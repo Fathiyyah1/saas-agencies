6e2aaf34a67d3fbefdf407ae15790499d4ba1b43
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-05-19 22:30:03 +0300

    h

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -30,7 +30,7 @@ export default async function Home() {
           </div>
     </section>
     <section className='flex justify-center items-center flex-col gap-4 mt-20'>
-      
+      <h2 className='text-4xl text-center'>Choose what fits you right </h2>
     </section>
     </>
   )
