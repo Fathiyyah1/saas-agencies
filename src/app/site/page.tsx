f1aae969ff17202019227d7e9244dfb39fd8d48c
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-05-20 14:12:07 +0300

    n

--- src/app/site/page.tsx
+++ src/app/site/page.tsx
@@ -31,7 +31,7 @@ export default async function Home() {
              bottom-0 top-[50%] bg-gradient-to-t dark:from-background left-0 right-0 absolute z-10'></div>
           </div>
     </section>
-    <section className='flex justify-center items-center flex-col gap-4 mt-20'>
+    <section className='flex justify-center items-center flex-col gap-4 md:!mt-20 mt-[-60px]'>
       <h2 className='text-4xl text-center'>Choose what fits you right </h2>
       <p className='text-muted-foreground text-center'>Our straightforward pricing plans are tailored to meet your needs. If {" you're"} not <br /> ready to commit you can get started for free</p>
       <div className='flex justify-center gap-4 flex-wrap mt-6'>
