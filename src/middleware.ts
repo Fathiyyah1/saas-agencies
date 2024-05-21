d4069aa8ffa4dea65cf6b8040a5bae6e80fee7ff
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-05-21 15:08:53 +0300

    important point

--- src/middleware.ts
+++ src/middleware.ts
@@ -2,6 +2,19 @@ import { authMiddleware } from "@clerk/nextjs/server";
 
 export default authMiddleware({
     publicRoutes: ['/site', '/api/uploadthing'],
+
+    async beforeAuth(auth, req){},
+    async afterAuth(auth, req){
+      //rewrite for domains 
+      const url = req.nextUrl
+      const searchParams = url.searchParams.toString()
+      let hostname = req.headers 
+
+      const pathWithSearchParams = `${url.pathname}${searchParams.length > 0 ? `?${searchParams}`:'' }`
+
+      //if subdomain exists
+    }
+
 });
 
 export const config = {
