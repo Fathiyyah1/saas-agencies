ee03663c59b09f793c656c60436f060ee92bc494
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-05-21 17:42:07 +0300

    new

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,13 @@
-import type { Metadata } from "next";
-import { DM_Sans } from "next/font/google";
-import "./globals.css";
-import { ThemeProvider } from "@/providers/theme-provider";
+import type { Metadata } from 'next'
+import { DM_Sans } from 'next/font/google'
+import './globals.css'
+import { ClerkProvider } from '@clerk/nextjs'
+import { dark } from '@clerk/themes'
+import { ThemeProvider } from '@/providers/theme-provider'
+import { Toaster } from '@/components/ui/toaster'
+import { Toaster as SonnarToaster } from '@/components/ui/sonner'
 
-const font = DM_Sans({ subsets: ["latin"] });
-
+const font = DM_Sans({ subsets: ['latin'] })
 export const metadata: Metadata = {
   title: "Agency",
   description: "All in one Agency Solution",
@@ -13,22 +16,27 @@ export const metadata: Metadata = {
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode;
+  children: React.ReactNode
 }) {
   return (
+    <html
+      lang="en"
+      suppressHydrationWarning
+    >
+      <body className={font.className}>
+        <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
+        >
 
-    <html lang="en" suppressHydrationWarning>
-    <body className={font.className}>
-      <ThemeProvider 
-      attribute="class"
-      defaultTheme="system"
-      enableSystem
-      disableTransitionOnChange
-      >
-    {children}
-  
-      </ThemeProvider> 
-         </body>
+            {children}
+            <Toaster />
+            <SonnarToaster position="bottom-left" />
+
+        </ThemeProvider>
+      </body>
     </html>
-  );
+  )
 }
