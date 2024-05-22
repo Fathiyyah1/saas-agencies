1e0106c57b2c811501b4b6dbb5f60cf23d65beb5
Fathiyyah1 <91325017+Fathiyyah1@users.noreply.github.com>
2024-05-23 02:59:25 +0300

    nn

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,1 +1,8 @@
 
+import { PrismaClient } from '@prisma/client'
+
+declare global {
+  var prisma: PrismaClient | undefined
+}
+ //this way no need to create multiple clients in every single reloads 
+export const db = globalThis.prisma || new PrismaClient()
